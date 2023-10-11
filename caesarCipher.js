bef52b51e45bbfbab7ab0eae08a2117dd5511346
Alexei Rusu <rusualexei1995@gmail.com>
2023-10-10 21:23:40 -0400

    Caesar Cipher

---
+++ caesarCipher.js
@@ -0,0 +1,27 @@
+// Submissions	Leaderboard	Discussions	Editorial
+// Julius Caesar protected his confidential information by encrypting it using a cipher. Caesar's cipher shifts each letter by a number of letters. If the shift takes you past the end of the alphabet, just rotate back to the front of the alphabet. In the case of a rotation by 3, w, x, y and z would map to z, a, b and c.
+
+
+function caesarCipher(s, k) {
+    // Write your code here
+    const alphabet = "abcdefghijklmnopqrstuvwxyz".split('')
+    const string = s.split('')
+    let enc = ''
+
+    for(let i = 0; i < string.length  ; i++){
+        if(string[i] === '-') { enc += `-`}
+        for(let j = 0; j < alphabet.length ; j++){
+            
+            if(string[i] === alphabet[j].toUpperCase()){
+                enc += `${alphabet[(j + k) % 26].toUpperCase()}`
+            } 
+            if(string[i] === alphabet[j]){
+                    enc += `${alphabet[(j + k) % 26]}` 
+            }
+        } 
+    }
+    return enc
+}
+
+
+console.log(caesarCipher('Always-Look-on-the-Bright-Side-of-Life', 5))   // output: Fqbfdx-Qttp-ts-ymj-Gwnlmy-Xnij-tk-Qnkj
